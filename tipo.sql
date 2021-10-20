17db6a5c5bcf2542b6fa2c975baeaf6e61495bf2
Rui Castelhano <rui.castelhano@cascaisambiente.pt>
2021-10-20 14:02:07 +0100

    Add files via upload

---
+++ tipo.sql
@@ -0,0 +1,5 @@
+insert into tipo (tipo) values ('Iguana, land');
+insert into tipo (tipo) values ('Greater blue-eared starling');
+insert into tipo (tipo) values ('Puku');
+insert into tipo (tipo) values ('Chilean flamingo');
+insert into tipo (tipo) values ('Snake, buttermilk');
