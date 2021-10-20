15f89f885a0e6facc596d8ff1b5064786fba9170
Rui Castelhano <rui.castelhano@cascaisambiente.pt>
2021-10-20 14:19:46 +0100

    Add files via upload

---
+++ veterinario.sql
@@ -0,0 +1,6 @@
+insert into veterinario (nome) values ('Regina');
+insert into veterinario (nome) values ('Imelda');
+insert into veterinario (nome) values ('Delilah');
+insert into veterinario (nome) values ('Tore');
+insert into veterinario (nome) values ('Geoff');
+insert into veterinario (nome) values ('Darbie');
